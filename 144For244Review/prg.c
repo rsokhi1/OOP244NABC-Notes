f9f361fd130253c1bcaea41cc935cd435ada6d13
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2021-05-13 17:01:07 -0400

    Review Session Codes.

--- 144For244Review/prg.c
+++ 144For244Review/prg.c
@@ -1,22 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-const double PI = 3.14159265;
+void flush() {
+   char ch = 0;
+   while (ch != '\n') {
+      scanf("%c", &ch);
+   }
+}
+void getNames(char n[][31], int num) {
+   int i;
+   for (i = 0; i < num; i++) {
+      printf("name %d: ", (i + 1));
+      scanf("%[^\n]", n[i]);
+      flush();
+   }
+}
+void saveName(char n[][31], int num) {
+   FILE* names = fopen("names.txt", "w");
+   int i;
+   for (i = 0; i < num; i++) {
+      fprintf(names, "%s\n", n[i]);
+   }
+   fclose(names);
+}
 int main() {
-   int a;
-   int b = 12;
-   int c;
-   a = b + 4;
-   printf("%d\n", a);
-   a = a + 10;
-   printf("%d\n", a);
-   a = b < 10;
-   printf("%d\n", a);
-   a = b > 10;
-   printf("%d\n", a);
-   c = a = b + 20;
-   printf("%d\n", a);
-   printf("%d\n", c);
-   a = !!b;
-   printf("%d\n", a);
+   char name[5][31];
+   getNames(name, 5);
+   saveName(name, 5);
    return 0;
 }
