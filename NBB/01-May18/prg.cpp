1eef7ad0bdec58a7414adbf0b838db9fa1a217e8
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2021-05-19 13:36:03 -0400

    Intro OOP

--- NBB/01-May18/prg.cpp
+++ NBB/01-May18/prg.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "StructStudent.h"
 using namespace std;
+
 int main() {
-   cout << "Hello OOP244NBB!!!!" << endl;
+   struct Student sts[30];
+   int i;
+   for (i = 0; i < 30; i++) {
+      getStInfo(&sts[i]);
+   }
+    // some process
+   for (i = 0; i < 30; i++) {
+      prnStudent(&sts[i]);
+   }
+
    return 0;
 }
